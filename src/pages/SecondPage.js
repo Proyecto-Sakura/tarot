f4a570aa47c34880b8bb00c06e0cea507d9a8956
yenromard <yenifer-romero@hotmail.com>
2022-12-20 12:25:07 +0100

    create router and second page

---
+++ src/pages/SecondPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header'
+
+function SecondPage() {
+  return (
+    <div>
+        <Header/>
+    </div>
+  )
+}
+
+export default SecondPage

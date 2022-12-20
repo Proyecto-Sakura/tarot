c5c3b3a9ed8bbc43efd200444b790c50901ea252
MichelleParada <mymisshy@gmail.com>
2022-12-20 09:34:26 +0100

    style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,20 @@ import "../style/header.css"
 
 function Header() {
   return (
-    <div></div>
+    <nav>
+      <navbar>
+        <div className='namePage'>Divinity Cards</div>
+          <div>
+            <button className='buttonResult'>Ver resultado</button>
+          </div>
+      </navbar>
+      <div className='info'>
+        <div className='forYou'>"A ti mujer que quieres  ser creadora de tu propio 
+destino, aquí te traemos el Tarot que te revelará 
+sorpresas que te depara el futuro"</div>
+        <div className='instructions'>Instrucciones: Elije 1 carta que representa tu pasado, 1 carta que representa tu presente y que representará tu futuro</div>
+        </div>    
+    </nav>
   )
 }
 
